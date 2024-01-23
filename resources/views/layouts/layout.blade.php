054288ab594866a68b086f2772b01ba8d6917e30
EbraimSambo <ebraimsambo@gmail.com>
2024-01-23 20:16:07 +0000

    mobile

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title> {{ $titlePage ? $titlePage . ' - TopHit' : 'TopHit'  }} </title>
+        <title> {{ $titlePage ? $titlePage . ' - Ghetto Music' : 'Ghetto Music'  }} </title>
         <link rel="stylesheet" href="{{ url('assets/styles/css/global.css') }}">
         <link rel="shortcut icon" href="{{ url('assets/images/logos/logo-1-png.png') }}" type="image/x-icon">
 
